a22799befdcc0de6443888de38d557f1cbbb6ed2
Roger Mokarzel <roger.mokarzel@gmail.com>
2019-07-29 16:38:49 -0300

    sending mail with Adonis mail

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const crypto = require('crypto');
+const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -13,6 +14,17 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        message => {
+          message
+            .to(user.email)
+            .from('roger.mokarzel@gmail.com', 'Roger Mokarzel')
+            .subject('Password recovery')
+        }
+      )
     } catch (err) {
       return response.status(err.status).send({ error: { message: 'Something went wrong! Does this e-mail exists?' } })
     }
